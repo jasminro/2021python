8bafbe338da8472bc2f914109ac16670975d007f
Jasmin Rolig <jasmin.rolig@tuni.fi>
2021-01-22 15:17:59 +0200

    Adding files

---
+++ Examples/name.py
@@ -0,0 +1,5 @@
+# Ohjelma kysyy käyttäjän nimeä. Tulostaa esim "Mikä sinun nmesi on?",
+# Ohjelma myös muuttaa nimen alkukirjaimen isoksi ja tulostaa "Sinun nimesi on <Nimi>"
+
+name = input("Mikä sinun nimesi on? ")
+print("Sinun nimesi on", name.capitalize())
